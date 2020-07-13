9fc61c80880843fe31952ddaafddb1187b9eff33
wuchao <541847041@qq.com>
2020-07-13 11:48:35 +0800

    最后提交

---
+++ src/complex/MinimumValue.ts
@@ -0,0 +1,38 @@
+function MinimumValue(min: number = 0) {
+    // Scope the value to be reused
+    let value: number;
+    return (target: any, propertyKey: string | symbol) => {
+        // Store the definition result
+        const update = Reflect.defineProperty(
+            target,
+            propertyKey,
+            {
+                configurable: true,
+                enumerable: true,
+                get: () => {
+                    // Return the scoped value
+                    return value;
+                },
+                set: (newValue: number) => {
+                    // Update the scoped value with max(newValue, min)
+                    value = (
+                        newValue >= min ? newValue : min
+                    );
+                }
+            },
+        );
+        // If the update failed, something went wrong
+        if (!update) {
+            // Kill everything
+            throw new Error("Unable to update property");
+        }
+    }
+}
+
+class MinTest{
+    @MinimumValue(30)
+    public num: number = 60;
+}
+const minTest = new MinTest();
+console.log('num:',minTest.num)
+
